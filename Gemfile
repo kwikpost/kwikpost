4d45dbe5a8af411028dc4dab2dd151fd4d0482a2
Malik Nur <malik.dba.pro@gmail.com>
2015-08-25 16:15:29 -0700

    paperclip config

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ gem 'omniauth-facebook'
 gem 'mailboxer'
 gem 'chosen-rails'
 
-gem 'aws-sdk'
+gem 'aws-sdk', '< 2.0'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,14 +1,16 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
 config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
+    :storage => :s3,
+    :s3_protocol => 'http',
     :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :s3_credentials => {
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
-}
 
   # Code is not reloaded between requests.
   config.cache_classes = true
