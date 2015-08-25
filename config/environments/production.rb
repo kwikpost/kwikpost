7358bdd9c948a6ffa989d2346e4d546661b138f2
Malik Nur <malik.dba.pro@gmail.com>
2015-08-25 14:25:01 -0700

    aws conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,15 +1,14 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.paperclip_defaults = {
+config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
+    :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
   }
-
+}
 
   # Code is not reloaded between requests.
   config.cache_classes = true
