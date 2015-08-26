238868568d1cefc7cdf2b7d3284240a0e93451fc
Austin Chang <achang0406@gmail.com>
2015-08-26 14:57:49 -0700

    moved product create from mains controller to products controller. Added buy, ask, watch buttons

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,7 +13,16 @@ class ProductsController < ApplicationController
   end
 
   def create
+    product = Product.new(product_params)
+    product.status = true
+    product.user_id = current_user.id
 
+    if product.save
+      flash[:errors] = ["Successfully added a new product!"]
+    else
+      flash[:errors] = product.errors.full_messages
+    end
+    redirect_to "/mains/#{current_user.id}/posts"
   end
 
   def update
@@ -32,6 +41,7 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-
+    params.require(:product).permit(:title, :price, :description, :price_fixed, :category_id, :condition_id, :avatar)
   end
+
 end
