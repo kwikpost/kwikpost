74f29115d5635d3f37b7b15fd09bb3fab5cc64e1
Chris Tran <christopher.huytran@gmail.com>
2015-08-28 00:03:46 -0700

    ip detect

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,6 +18,7 @@ class ProductsController < ApplicationController
 
     if params[:search_location].present?
       @products = Product.near(params[:search_location], 50).paginate(:page => params[:page], :per_page => 20)
+      @location = params[:search_location]
     else
       if params[:category_id]
         @products = Product.where(:category_id => params[:category_id]).paginate(:page => params[:page], :per_page => 20)
